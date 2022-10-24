3b2b6cd2eae7080377685529d2cf2a28b44aaafa
Shiyas-N <muhammedshiyasn811@gmail.com>
2022-10-24 07:36:55 +0530

    add css

--- Task 02.js
+++ Task 02.js
@@ -2,14 +2,17 @@ const ReactContent = document.getElementById("root");
 
 const Elmnt = () =>{
     return(
-        <ul>
-            <li>MANGO</li>
-            <li>BANANA</li>
-            <li>APPLE</li>
-            <li>ORANGE</li>
-            <li>GRAPE</li>
-            <li>WATERMELON</li>
-        </ul>
+        <div>
+            <h2 align="center">FRUITS</h2>
+            <ul>
+              <li>MANGO</li>
+              <li>BANANA</li>
+              <li>APPLE</li>
+              <li>ORANGE</li>
+              <li>GRAPE</li>
+              <li>WATERMELON</li>
+            </ul>
+        </div>
     )
 }
 ReactDOM.render(<Elmnt />,ReactContent)
